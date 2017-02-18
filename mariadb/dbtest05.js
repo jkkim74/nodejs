48964da6df4260777bd32023cad24716e1c59b09
jkkim74 <jack68@naver.com>
2017-02-18 23:37:41 +0900

    mariadb + node.js

---
+++ mariadb/dbtest05.js
@@ -0,0 +1,16 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'devuser',
+  password : 'okpassokpass',
+  database : 'okdevdb'
+});
+
+connection.connect();
+
+var query = connection.query(
+  'DELETE FROM user WHERE id = ?', 
+  [ 1 ], function(err, result) {
+  console.log(result);
+});
+connection.end();
