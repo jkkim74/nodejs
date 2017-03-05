66769d5d406a42197e780b752e00e7c8041b2268
jkkim74 <jack68@naver.com>
2017-03-05 14:27:44 +0900

    ejs

---
+++ cluster/single.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+// Workers can share any TCP connection
+// In this case it is an HTTP server
+http.createServer((req, res) => {
+res.writeHead(200);
+res.end('hello world\n');
+}).listen(8000);
